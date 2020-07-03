b3e0e8702397a642646aac7536e7fd9fe26ade8d
Jorge <jrivero@utexas.edu>
2020-07-03 16:34:06 -0600

    Added detail menus for food options

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -14,22 +14,32 @@ struct ItemRow: View {
     
     var body: some View {
         
-        HStack{
-            Image(item.thumbnailImage).clipShape(Circle()).overlay(Circle().stroke(Color.gray, lineWidth: 2))
-            
-            VStack(alignment: .leading){
-                Text(item.name).font(.headline)
-                Text(String("$\(item.price)"))
-            }
-            ForEach(item.restrictions, id:\.self){ restriction in
-                Text(restriction)
-                    .font(.caption).fontWeight(.black).padding(5).clipShape(Circle()).foregroundColor(.white).background(Self.colors[restriction, default: .black])
+        NavigationLink(destination: ItemDetail(item: item)){
+        
+            HStack{
+                Image(item.thumbnailImage).clipShape(Circle()).overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                
+                VStack(alignment: .leading){
+                    Text(item.name).font(.headline)
+                    Text(String("$\(item.price)"))
+                }
+                Spacer()
+                
+                ForEach(item.restrictions, id:\.self){ restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                        
+                    
+                }
                 
             }
-            
+        
         }
-        
-        
     }
 }
 
