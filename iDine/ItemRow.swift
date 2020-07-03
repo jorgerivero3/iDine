291dd45d72ecb10a3499496477248bde673ec6bb
Jorge Rivero III <jrivero@jorges-air.lan>
2020-07-03 13:06:40 -0600

    First commit

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,21 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Jorge Rivero III on 5/9/20.
+//  Copyright © 2020 Jorge Rivero III. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}
