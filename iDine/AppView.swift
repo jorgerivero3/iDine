0c3ae0f3cd06bc7803a80b7d9202f504b826f31d
Jorge <jrivero@utexas.edu>
2020-07-03 18:28:51 -0600

    Checkout functionality

---
+++ iDine/AppView.swift
@@ -0,0 +1,34 @@
+//
+//  AppView.swift
+//  iDine
+//
+//  Created by Jorge Rivero III on 7/3/20.
+//  Copyright © 2020 Jorge Rivero III. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Menu")
+                }
+
+            OrderView()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Order")
+                }
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        AppView().environmentObject(order)
+    }
+}
---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,68 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Jorge Rivero III on 7/3/20.
+//  Copyright © 2020 Jorge Rivero III. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 1
+    @State private var paymentType = 0
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var showingPaymentAlert = false
+    var totalPrice: Double{
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
+    
+    
+    
+    var body: some View {
+        Form{
+            Section{
+                Picker("How do you want to pay", selection: $paymentType){
+                    ForEach(0..<Self.paymentTypes.count){
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+                Toggle(isOn: $addLoyaltyDetails.animation()){
+                    Text("Add iDine Loyalty Card")
+                }
+                if addLoyaltyDetails{
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            Section(header: Text("Add a tip?")){
+                Picker("Percentage", selection: $tipAmount){
+                    ForEach(0..<Self.tipAmounts.count){
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header: Text("TOTAL: $\(totalPrice, specifier: "%.2f")").font(.largeTitle)){
+                Button("Confirm Order"){
+                    self.showingPaymentAlert.toggle()
+                }
+            }
+        }.navigationBarTitle(Text("Payment"), displayMode: .inline)
+            .alert(isPresented: $showingPaymentAlert){
+                Alert(title: Text("Order Confirmed"), message: Text("Your total was $\(totalPrice, specifier: "%.2f") – thank you!"), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
